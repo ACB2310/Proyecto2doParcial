1e5c111a11eb1d2e103efa8a20a0d228e621a308
ACB2310 <lrs22110100@purisima.tecnm.mx>
2026-04-23 07:36:05 -0600

    Navigation Corrections

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import NavigationStack from "./src/Navigation/NavigationStack";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <NavigationStack />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
--- src/Navigation/NavigationStack.js
+++ src/Navigation/NavigationStack.js
@@ -1,1 +1,27 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import ProductDetailsScreen from "../Screens/Products/ProductDetailsScreen";
+import ProductListScreen from "../Screens/Products/ProductListScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function NavigationStack() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="ProductList">
+        <Stack.Screen
+          name="ProductList"
+          component={ProductListScreen}
+          options={{ title: "Productos" }}
+        />
+        <Stack.Screen
+          name="ProductDetails"
+          component={ProductDetailsScreen}
+          options={{ title: "Detalle del producto" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
